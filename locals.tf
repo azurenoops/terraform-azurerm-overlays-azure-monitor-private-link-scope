6947c2d624954368b9cd5d463ea73a4695d38102
John Spinella <jrspinella@users.noreply.github.com>
2024-03-29 16:04:55 -0400

    Updates to Private Links

--- locals.tf
+++ locals.tf
@@ -4,11 +4,17 @@
 # remove file if not needed
 
 locals {
-  private_dns_zones_names = [
+  private_dns_zones_names = var.environment == "public" ? [
     "privatelink.agentsvc.azure-automation.net",
     "privatelink.blob.core.windows.net",
     "privatelink.monitor.azure.com",
     "privatelink.ods.opinsights.azure.com",
     "privatelink.oms.opinsights.azure.com",
+    ] : [
+    "privatelink.oms.opinsights.azure.us",
+    "privatelink.ods.opinsights.azure.us",
+    "privatelink.agentsvc.azure-automation.us",
+    "privatelink.blob.core.usgovcloudapi.net",
+    "privatelink.blob.core.usgovcloudapi.net",
   ]
 }
